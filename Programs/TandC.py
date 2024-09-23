238da5c332961ba29aa58bac244c3b9ad21fdad7
yazhunjul <yazhunjul@gmail.com>
2024-09-23 16:20:31 -0400

    Added many features

--- Programs/TandC.py
+++ Programs/TandC.py
@@ -1,6 +1,6 @@
 from Programs import genInfo as GI, canopy as canopy
 import openpyxl
-from openpyxl.styles import Font, Border, Side, PatternFill
+from openpyxl.styles import Font, Border, Side, PatternFill, Alignment
 import streamlit as st
 
 def TandC():
@@ -43,21 +43,205 @@ def saveToExcel(genInfo, hoods):
 
     # Apply the fill and border dynamically to a range of cells
 
-
+    
     for k,v in hoods.items():
-        for col in range(1, 10):  # Column A to B (1 to 2)
-            cell = ws.cell(row=row, column=col)
-            cell.fill = fill_style  # Apply fill
-            cell.border = thin_border  # Apply border
-        ws[f'A{row}'].border = Border(left=Side(style='thin'),top=Side(style='thin'),
-        bottom=Side(style='thin'))
-        ws[f'j{row}'].border = Border(left=Side(style='thin'))
-        ws.row_dimensions[row].height = 20
-        genFont(ws, 'A', row, "EXTRACT AIR DATA")
-        row+=1
-        genFont(ws, 'A', row,v.drawingNum)
-        fillBorder(ws, 'A', row)
-        row+=1
+        #Capture Jet Hoods
+        if v.model in ['KVF', 'KVI', 'KCH-F', 'KCH-I']:
+            colorFill(ws, row)
+            ws[f'A{row}'].border = Border(top=Side(style='thin'),
+            bottom=Side(style='thin'))
+            ws[f'j{row}'].border = Border(left=Side(style='thin'))
+            ws.row_dimensions[row].height = 20
+            ws.merge_cells(f'A{row}:I{row}')
+            genFont(ws, 'A', row, "EXTRACT AIR DATA")
+            makeCenter(ws, 'A', row)
+            row+=1
+            genFont(ws, 'A', row,f'Drawing Number: \t{v.drawingNum}')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            genFont(ws, 'A', row, f'Location: \t{v.location}')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            genFont(ws, 'A', row, f'Model: \t{v.model}')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            genFont(ws, 'A', row, f'Quantity of Canopy Sections: \t{v.quantityOfSections}')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            genFont(ws, 'A', row, f'Calculation: \tQV = Kf x √Pa')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            colorFill(ws, row)
+            ws.merge_cells(f'A{row}:I{row}')
+            genFont(ws, 'A', row, "EXTRACT AIR READINGS")
+            makeCenter(ws, 'A', row)
+            row+=1
+            #Module Number
+            #ws.merge_cells(f'A{row}:B{row}')
+            extendRow(ws, row)
+            genFont(ws, 'A', row, 'Module #')
+            makeCenter(ws, 'A', row)
+            sectionBorder(ws, row, 1,2)
+            #TAB Point Reading
+            ws.merge_cells(f'B{row}:C{row}')
+            extendRow(ws, row)
+            genFont(ws, 'B', row, 'T.A.B Point Reading (Pa)')
+            makeCenter(ws, 'B', row)
+            sectionBorder(ws, row, 2,4)
+            #K-Factor (m3/h)
+            ws.merge_cells(f'D{row}:E{row}')
+            extendRow(ws, row)
+            genFont(ws, 'D', row, 'K-Factor (m3 /h)')
+            makeCenter(ws, 'D', row)
+            sectionBorder(ws, row, 4,6)
+            #Achieved Flowrate
+            ws.merge_cells(f'F{row}:G{row}')
+            extendRow(ws, row)
+            genFont(ws, 'F', row, 'Flowrate (m3 /h)')
+            makeCenter(ws, 'F', row)
+            sectionBorder(ws, row, 6,8)
+            #Design Flowrate
+            ws.merge_cells(f'H{row}:I{row}')
+            extendRow(ws, row)
+            genFont(ws, 'H', row, 'Flowrate (m3 /s)')
+            makeCenter(ws, 'H', row)
+            sectionBorder(ws, row, 8,10)
+            row+=1
+            # Now Fill the Info (v.sections)
+            print(v.sections)
+            totalFlowRate = 0
+            for section, info in v.sections.items():
+                print(info)
+                print(f'{section} : {info}')
+                #Mod Number
+                genFont(ws, 'A', row, section)
+                makeCenter(ws, 'A', row)
+                sectionBorder(ws, row, 1,2)
+                #Tab Reading
+                ws.merge_cells(f'B{row}:C{row}')
+                genFont(ws, 'B', row, f' {info['tab_reading']} pa')
+                makeCenter(ws, 'B', row)
+                sectionBorder(ws, row, 2, 4)
+                #K-Factor
+                ws.merge_cells(f'D{row}:E{row}')
+                genFont(ws, 'D', row, f' {info['k_factor']}')
+                makeCenter(ws, 'D', row)
+                sectionBorder(ws, row, 4, 6)
+                #AchievedFlow
+                ws.merge_cells(f'F{row}:G{row}')
+                genFont(ws, 'F', row, f' {info['achieved']}')
+                makeCenter(ws, 'F', row)
+                sectionBorder(ws, row, 6, 8)
+                #Design Flow
+                ws.merge_cells(f'H{row}:I{row}')
+                genFont(ws, 'H', row, f' {info['designFlow']}')
+                makeCenter(ws, 'H', row)
+                sectionBorder(ws, row, 8, 10)
+                totalFlowRate += info['designFlow']
+                row+=1
+                #Total Flow Rate
+        
+            ws.merge_cells(f'F{row}:I{row}')    
+            genFont(ws, 'F', row, f'Total Flowrate                                {totalFlowRate} m3/s')
+            sectionBorder(ws, row, 6, 10)
+            row +=2
+            colorFill(ws, row)
+            ws.merge_cells(f'A{row}:I{row}')
+            genFont(ws, 'A', row, "SUPPLY AIR DATA")
+            makeCenter(ws, 'A', row)
+            row+=1
+            genFont(ws, 'A', row,f'Drawing Number: \t{v.drawingNum}')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            genFont(ws, 'A', row, f'Location: \t{v.location}')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            genFont(ws, 'A', row, f'Model: \t{v.model}')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            genFont(ws, 'A', row, f'Quantity of Canopy Sections: \t{v.quantityOfSections}')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            genFont(ws, 'A', row, f'Calculation: \tQV = Kf x √Pa')
+            fillBorder(ws, 'A', row)
+            iBorder(ws, 'i', row)
+            row+=1
+            extendRow(ws, row)
+            genFont(ws, 'A', row, 'Module #')
+            makeCenter(ws, 'A', row)
+            sectionBorder(ws, row, 1,2)
+            #TAB Point Reading
+            ws.merge_cells(f'B{row}:C{row}')
+            extendRow(ws, row)
+            genFont(ws, 'B', row, 'T.A.B Point Reading (Pa)')
+            makeCenter(ws, 'B', row)
+            sectionBorder(ws, row, 2,4)
+            #K-Factor (m3/h)
+            ws.merge_cells(f'D{row}:E{row}')
+            extendRow(ws, row)
+            genFont(ws, 'D', row, 'K-Factor (m3 /h)')
+            makeCenter(ws, 'D', row)
+            sectionBorder(ws, row, 4,6)
+            #Achieved Flowrate
+            ws.merge_cells(f'F{row}:G{row}')
+            extendRow(ws, row)
+            genFont(ws, 'F', row, 'Flowrate (m3 /h)')
+            makeCenter(ws, 'F', row)
+            sectionBorder(ws, row, 6,8)
+            #Design Flowrate
+            ws.merge_cells(f'H{row}:I{row}')
+            extendRow(ws, row)
+            genFont(ws, 'H', row, 'Flowrate (m3 /s)')
+            makeCenter(ws, 'H', row)
+            sectionBorder(ws, row, 8,10)
+            row+=1
+            totalFlowRateSup = 0
+            for section2, info2 in v.sections.items():
+                #Mod Number
+                print(v.sections.items())
+                genFont(ws, 'A', row, section2)
+                makeCenter(ws, 'A', row)
+                sectionBorder(ws, row, 1,2)
+                #Tab Reading
+                ws.merge_cells(f'B{row}:C{row}')
+                genFont(ws, 'B', row, f' {info2['supplyTab']} pa')
+                makeCenter(ws, 'B', row)
+                sectionBorder(ws, row, 2, 4)
+                #K-Factor
+                ws.merge_cells(f'D{row}:E{row}')
+                genFont(ws, 'D', row, f' {info2['supplyKFactor']}')
+                makeCenter(ws, 'D', row)
+                sectionBorder(ws, row, 4, 6)
+                #AchievedFlow
+                ws.merge_cells(f'F{row}:G{row}')
+                genFont(ws, 'F', row, f' {info2['achievedSupply']}')
+                makeCenter(ws, 'F', row)
+                sectionBorder(ws, row, 6, 8)
+                #Design Flow
+                ws.merge_cells(f'H{row}:I{row}')
+                genFont(ws, 'H', row, f' {info2['supplyDesign']}')
+                makeCenter(ws, 'H', row)
+                sectionBorder(ws, row, 8, 10)
+                totalFlowRateSup += info2['supplyDesign']
+                row+=1
+            ws.merge_cells(f'F{row}:I{row}')    
+            genFont(ws, 'F', row, f'Total Flowrate                                {totalFlowRateSup} m3/s')
+            sectionBorder(ws, row, 6, 10)
+            row +=2
+        ws.row_breaks.append(openpyxl.worksheet.pagebreak.Break(id=row))  # Break after Row 20 (before row 21)
+            
+            
+            #End of Capture Jet Hoods
+        row+=2
     
 
     # You can also add more rows if necessary, e.g., canopy details, etc.
@@ -71,11 +255,28 @@ def saveToExcel(genInfo, hoods):
     # Provide the download link
     with open(updated_workbook_path, "rb") as file:
         st.download_button(label=f"Download Report", data=file, file_name=f"Testing And Commissioning.xlsx")
-        
+      
+def iBorder(ws, letter, row):
+    ws[f'i{row}'].border = Border(right=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))
+    ws[f'A{row}'].border = Border(left=Side(style='thin'), top=Side(style='thin'), bottom=Side(style='thin'))
 def genFont(ws, letter, row, word):
     ws[f'{letter}{row}'] = word
     ws[f'{letter}{row}'].font = Font(size=11, bold=True)
-    
+def extendRow(ws, row):
+    ws.row_dimensions[row].height = 30
+def makeCenter(ws, letter, row):
+    ws[f'{letter}{row}'].alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
+def sectionBorder(ws,row, first, second):
+    thin_border = Border(
+    top=Side(style='thin'),
+    bottom=Side(style='thin'),
+    left=Side(style='thin'),
+    right=Side(style='thin')
+    )
+    for col in range(first, second):  # Columns A to D
+        cell = ws.cell(row=row, column=col)
+        cell.border = thin_border
+        
 def titleFont(ws, letter, row, word):
     ws[f'{letter}{row}'] = word
     ws[f'{letter}{row}'].font = Font(size=14, bold=True)
@@ -88,3 +289,15 @@ def fillBorder(ws, letter, row):
     for col in range(1, 10):  # Column A to B (1 to 2)
             cell = ws.cell(row=row, column=col)
             cell.border = thin_border  # Apply border
+            
+def colorFill(ws, row):
+    fill_style = PatternFill(start_color="9ac9f4", end_color="9ac9f4", fill_type="solid")
+# Define a border (thin border for all sides)
+    thin_border = Border(
+    top=Side(style='thin'),
+    bottom=Side(style='thin')
+)
+    for col in range(1, 10):  # Column A to B (1 to 2)
+                cell = ws.cell(row=row, column=col)
+                cell.fill = fill_style  # Apply fill
+                cell.border = thin_border  # Apply border
--- Programs/canopy.py
+++ Programs/canopy.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import math
 cj_hoods = ['KVF', 'KVI', 'KCH-F', 'KCH-I']
 
 cj_hoods_k_factor =  {
@@ -9,6 +9,14 @@ cj_hoods_k_factor =  {
     4 : 287.2,
     5 : 359,
     6 : 430.8
+}
+
+cj_hoods_supply_kfactor = {
+    1000 : 121.7,
+    1500 : 182.6,
+    2000 : 243.4,
+    2500 : 304.2,
+    3000 : 365.1
 }
 
 class cjHoods():
@@ -21,11 +29,20 @@ class cjHoods():
         self.sections = {}
         self.k_factor = 0
         self.tab_Reading = 0
+        self.plenumLength = 0
+        self.design_supply = 0
+        self.tab_Reading_supply = 0
     
     def getSections(self, selection, num):
         st.markdown("<hr style='border:1px solid white;'>", unsafe_allow_html=True)
         for i in range(self.quantityOfSections):
+            st.markdown(f"<h4 style='text-align: center;margin-top: 30px;margin-bottom:-62px;'>{self.model} Section {i+1} Extract Info ({self.location})<h4>", unsafe_allow_html=True)
             col1, col2 = st.columns(2)
+            st.markdown("<hr style='border:1px solid white;'>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align: center;margin-top: 30px;margin-bottom:-62px;'>{self.model} Section {i+1} Supply Info ({self.location})<h4>", unsafe_allow_html=True)
+            col3, col4 = st.columns(2)
+            st.markdown("<hr style='border:1px solid white;'>", unsafe_allow_html=True)
+            
             with col1:
                 st.markdown(f"<h4 style='text-align: center;margin-top: 30px;margin-bottom:-62px;'>Section {i+1} KSA's<h4>", unsafe_allow_html=True)
                 self.ksaQuantity = st.number_input('.', key=f'ksaQuantity{selection}{num}{i}', label_visibility='hidden', min_value=1, max_value=6)
@@ -34,11 +51,24 @@ class cjHoods():
                 self.tab_Reading = st.number_input('.', key=f'canopyLocs{selection}{num}{i}', label_visibility='hidden', min_value=0.0)
             with col1:
                 st.markdown(f"<h4 style='text-align: center;margin-top: 30px;margin-bottom:-70px;'>Section {i+1} Design Airflow<h4>", unsafe_allow_html=True)
-                self.tab_Reading = st.number_input('.', key=f'designflow{selection}{num}{i}', label_visibility='hidden', min_value=0.0)
+                self.design_flow = st.number_input('.', key=f'designflow{selection}{num}{i}', label_visibility='hidden', min_value=0.0)
+            with col3:
+                st.markdown(f"<h4 style='text-align: center;margin-top: 30px;margin-bottom:-62px;'>Section {i+1} Plenum Length<h4>", unsafe_allow_html=True)
+                self.plenumLength = st.selectbox('.', [1000, 1500, 2000, 2500, 3000],key=f'plenumLength{selection}{num}{i}', label_visibility='hidden')
+            supplyKFactor = cj_hoods_supply_kfactor[self.plenumLength]
+            with col4:
+                st.markdown(f"<h4 style='text-align: center;margin-top: 30px;margin-bottom:-60px;'>Section {i+1} T.A.B Point Reading<h4>", unsafe_allow_html=True)
+                self.tab_Reading_supply = st.number_input('.', key=f'sup_tab{selection}{num}{i}', label_visibility='hidden', min_value=0.0)
+            with col3:
+                st.markdown(f"<h4 style='text-align: center;margin-top: 30px;margin-bottom:-60px;'>Section {i+1} Design Flow <h4>", unsafe_allow_html=True)
+                self.design_supply = st.number_input('.', key=f'design_sup{selection}{num}{i}', label_visibility='hidden', min_value=0.0)
+                
             self.k_factor = cj_hoods_k_factor[self.ksaQuantity]
+            achievedFlowRate = self.k_factor * math.sqrt(self.tab_Reading)
+            supplyAchievedFlowRate = cj_hoods_supply_kfactor[self.plenumLength] * math.sqrt(self.tab_Reading_supply)
+            print(math.sqrt(self.tab_Reading))
+            self.sections[f'{i+1}'] = {"ksaQuantity" : self.ksaQuantity, "k_factor" : self.k_factor, 'tab_reading' : self.tab_Reading, 'designFlow' : self.design_flow, 'achieved' : round(achievedFlowRate,2), 'supplyKFactor' : cj_hoods_supply_kfactor[self.plenumLength], 'supplyTab' : self.tab_Reading_supply, 'supplyDesign' : round(self.design_supply,2), 'achievedSupply' : supplyAchievedFlowRate}
             
-            self.sections[f'section {i+1}'] = {"ksaQuantity" : self.ksaQuantity, "k_factor" : self.k_factor, 'tab_reading' : self.tab_Reading}
-            st.markdown("<hr style='border:1px solid white;'>", unsafe_allow_html=True)
     
             
     
